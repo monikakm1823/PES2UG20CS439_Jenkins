340c59506dbb219e65844b3eab2cd0a6f9fe7c40
monikakm1823 <118333004+monikakm1823@users.noreply.github.com>
2023-02-16 10:07:11 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "SRN- PES2UG20CS439\n";
+  cout << "SRN-PES2UG20CS439\n";
   return 0;
 }
